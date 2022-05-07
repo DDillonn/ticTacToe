dc1acab433d87030088858b23ed06919216d51ba
DDillonn <Saltnessdillon@gmail.com>
2022-05-07 00:18:55 +0000

    Created Repo

---
+++ src/js/component/game.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const Game = () => {
+	return <div>tic tac toe</div>;
+};
+
+export default Game;
